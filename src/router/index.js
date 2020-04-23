20b3cd2964227665c1dc9fe935a92bc111966b04
jlozan64 <janeth2.jl@gmail.com>
2020-04-23 17:07:33 -0500

    cmponents

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import Login from "../views/Login.vue"
+import MyOrders from "../views/MyOrders.vue"
+import Cart from "../views/Cart.vue"
+import Inventory from "../views/Inventory.vue"
+
+import store from "../store"
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/myorders", name: "MyOrders", component: MyOrders, meta: { requiresRole: "user" } },
+  { path: "/cart", name: "Cart", component: Cart, meta: { requiresRole: "user" } },
+  { path: "/inventory", name: "Inventory", component: Inventory, meta: { requiresRole: "admin" } },
+]
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresRole)) {
+    if (store.getters.getUser.roles && store.getters.getUser.roles[to.meta.requiresRole]) {
+      next()
+    } else {
+      next("/login")
+    }
+  } else {
+    next() 
+  }
+})
+
+export default router
